a500dbb31964fca5320c9b21a454b6b78042c60e
Jeffrey Ness <jeffrey.ness@bigrentz.com>
2015-03-16 17:38:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# Monotool
+
+A tool for assisting with the Mono build process.
+
+## Installation
+
+```
+# If your are on OSX you can install mono and 
+# xbuild from brew:
+brew mono
+
+# Install nuget the package manager for the
+# Microsoft development platform
+sudo wget http://nuget.org/nuget.exe -O /usr/local/bin/nuget.exe 
+
+# Install Python virtualenv and pip 
+# http://docs.python-guide.org/en/latest/dev/virtualenvs/
+sudo easy_install pip
+sudo pip install virtualenv
+
+# Get the latest monotool source from Github:
+git clone git@github.com:bigrentz/monotool.git
+cd monotool/
+
+# Copy the example config to your user profile:
+cp monotool.conf.example ~/.monotool.conf
+
+# Create a activate Python virtualenv for site-packages:
+virtualenv ~/virtualenv/monotool
+source ~/virtualenv/monotool/bin/activate
+
+# Install monotool!
+python setup.py install
+```
+
+## Usage
+
+### Activating the monotool virtualenv
+
+If you have not already activated your virtualenv do it now
+
+```
+$ source ~/virtualenv/monotool/bin/activate
+```
+
+You can deactivate by either exiting the command session, 
+or running the following:
+
+```
+$ deactivate
+```
+
+### Getting help
+
+```
+$ monotool --help
+usage: monotool [-h] [--version] [--debug] [-s solution_file]  ...
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --version           show program's version number and exit
+  --debug
+  -s solution_file    A solution file to work with.
+
+Commands:
+
+    listProjects      Lists all project files found in the solution.
+    listProjectVersion
+                      Lists the projects Assembly Version.
+    listArtifacts     Lists all artifacts files found in the project.
+    copyArtifacts     Copies all artifacts files found in the project to dest.
+    deleteArtifacts   Deletes all artifacts from solution path.
+    nugetRestore      Run nuget restore on a solution file
+    xbuild            xbuild on a solution file
+```
+
+### Getting version
+
+```
+$ monotool --version
+0.0.5
+```
+
+### List all project definded in the Solution file
+
+```
+$ monotool -s BigRentz.Project.sln listProjects
+BigRentz.Project
+```
+
+### Get a project Assembly Version
+
+```
+$ monotool -s BigRentz.Project.sln listProjectVersion BigRentz.Project
+1.0.0.0
+```
+
+### Debug or verbose logging with the --debug flag
+
+```
+$ monotool --debug -s BigRentz.Project.sln listProjects
+2015-03-19 07:46:51,770 - DEBUG - Looking for project definitions in BigRentz.Project.sln
+BigRentz.Project
+```
+
+### Run Nuget Restore on a solution file.
+
+```
+$ monotool -s BigRentz.Project.sln nugetRestore
+2015-03-19 07:48:18,610 - INFO - Running: /usr/bin/mono /Users/jeffreyness/Downloads/NuGet.exe restore BigRentz.Project.sln
+2015-03-19 07:48:18,610 - INFO - This can take some time...
+2015-03-19 07:48:26,621 - INFO - Command Successful
+```
+
+### Running xbuild on a solution file.
+
+```
+$ monotool -s BigRentz.Project.sln xbuild
+2015-03-19 07:48:44,936 - INFO - Running: /usr/bin/xbuild BigRentz.Project.sln
+2015-03-19 07:48:44,937 - INFO - This can take some time...
+2015-03-19 07:48:46,625 - INFO - Command Successful
+```
+
+### Listing build artifacts.
+
+```
+$ monotool -s BigRentz.Project.sln listArtifacts
+BigRentz.Project
+  BigRentz.Project/bin/Debug/BigRentz.Project.dll
+```
+
+### Copy artifacts to a versioned directory
+
+```
+$ monotool -s BigRentz.Project.sln copyArtifacts tmp/
+2015-03-19 07:49:40,095 - INFO - Copying BigRentz.Project.dll to tmp/BigRentz.Project.0.0.0.1/
+```
+
+### Cleaning up
+
+```
+$ monotool -s BigRentz.Identity.sln clean
+2015-03-20 10:53:02,272 - INFO - Running: /usr/local/bin/xbuild BigRentz.Project.sln /t:clean
+2015-03-20 10:53:02,571 - INFO - Command Successful
+
--- monotool/monotool.py
+++ monotool/monotool.py
@@ -3,16 +3,18 @@
 from glob import glob
 from ConfigParser import ConfigParser
 from subprocess import Popen, PIPE
+from re import search
 
 import pkg_resources
 import argparse
 import logging
+import shutil
 import sys
 import os
 
 class MonoTool(object):
 
-    def __init__(self, repo_dir, debug=False):
+    def __init__(self, solution_file, debug=False):
         """
         repo_dir: Directory of the cloned repo.
         """
@@ -22,8 +24,18 @@ class MonoTool(object):
             self.log_level = 'INFO'
         self.logger = self.__logger()
         self.config = self.__config()
-        self.repo_dir = repo_dir.rstrip('/')
-        self.source_dir = os.path.abspath('%s/%s' % (self.repo_dir, self.config.get('sln_path', 'src')))
+
+        if os.path.exists(solution_file):
+            self.solution_file = solution_file.rstrip()
+        else:
+            raise Exception('Solution file %s not found' % solution_file)
+        self.solution_path = '/'.join(os.path.abspath(solution_file).split('/')[:-1])
+
+        # define a couple of our configs
+        self.output_path = self.config['output_path']
+        self.xbuild_path = self.config['xbuild_path']
+        self.mono_path = self.config['mono_path']
+        self.nuget_path = self.config['nuget_path']
 
     def __logger(self):
         _format = '%(asctime)s - %(levelname)s - %(message)s'
@@ -42,66 +54,183 @@ class MonoTool(object):
         else:
             raise Exception('Unable to open %s' % cfg)
 
-    def run(self, command, cwd='.'):
-        "Runs a command and returns a tuple of stdout and stderr"
+    def __run(self, command, cwd='.'):
+        """
+        Runs a command and returns a tuple of stdout and stderr
+        """
         process = Popen(command, cwd=cwd, shell=True, stdout=PIPE, stderr=PIPE)
-        process.wait()
         stdout, stderr = process.communicate()
-        if process.returncode:
-            self.logger.error(stderr)
-            raise Exception('Process returned with a non 0 return code.')
-        return stdout, stderr
+        return stdout, stderr, process.returncode
 
-    def __get_project_path(self, solution_file):
+    def __log_run(self, results):
         """
-        Get the solution path for a given sln file.
+        Logs the results of our __run using a universal method
+        """
+        print 'I made it'
+        stdout, stderr, returncode = results
 
-        A fancy extension remover =)
-        """
-        return '.'.join(solution_file.split('.')[0:-1])
-
-    def get_solutions(self):
-        """
-        Returns a list of solutions files in your
-        slnPath path.
-        """
-        self.logger.debug('Looking for sln files in %s' % (
-            self.source_dir)
-        )
-        solutions = glob('%s/*.sln' % (self.source_dir))
-        return [ (s, self.__get_project_path(s)) for s in solutions ]
-
-    def run_nuget(self, solution):
-        """
-        Run Nuget.exe if we have a packages.json
-        """
-        solution_file, project_path = solution
-        if os.path.exists('%s/packages.config' % project_path):
-            cmd = '%s %s restore %s' % (
-                    self.config['mono_path'], self.config['nuget_path'], solution_file
-            )
-            self.logger.debug('Running: %s' % cmd)
-            return self.run(cmd, cwd=self.source_dir)
+        if returncode:
+            if stdout:
+                self.logger.error(stdout)
+            if stderr:
+                self.logger.error(stderr)
+            raise Exception('Command returned with %s return code' % returncode)
         else:
-            self.logger.debug('Project does not have a %s/packages.json, skipping..' % project_path)
+            if stdout:
+                self.logger.debug(stdout)
+            if stderr:
+                self.logger.warn(stderr)
+            self.logger.info('Command Successful')
 
-    def run_xbuild(self, solution):
+    def __get_projects(self):
         """
-        Run xbuild on the solution
+        Gets the project defined by reading a solution file.
         """
-        solution_file, project_path = solution
-        cmd = '%s %s' % (self.config['xbuild_path'], solution_file)
-        self.logger.debug('Running: %s' % cmd)
-        return self.run(cmd, cwd=self.source_dir)
+        projects = []
+        self.logger.debug('Looking for project definitions in %s' % self.solution_file)
+        solution_blob = open(self.solution_file, 'r').read()
+        for line in solution_blob.split('\n'):
+            if 'Project(' in line:
+                _, _, project, csproj, _ = line.split()
 
-    def get_artifacts(self, solution):
+                project = project.split('"')[1]  # Ghetto hack to remove string inception.
+                csproj = csproj.split('"')[1]  # Ghetto hack to remove string inception.
+                csproj = csproj.replace('\\', '/')  # Windows path to Linux..
+
+                # Need to get path from csproj
+                project_path = csproj.split('/')[:-1][0]
+                output_path = '%s/%s' % (project_path, self.output_path)
+                projects.append(dict(project=project, csproj=csproj, output_path=output_path,
+                    project_path=project_path))
+        return projects
+
+    def __get_project(self, project_name):
+        """
+        Get a project from project_name
+        """
+        for project in self.__get_projects():
+            if project.get('project') == project_name:
+                return project
+
+    def __get_artifacts(self):
         """
         Returns a list of file artifacts
         """
-        solution_file, project_path = solution
-        artifact_path = self.config['artifact_path']
-        artifacts = glob('%s/%s/*' % (project_path , artifact_path))
+        artifacts = []
+        projects = self.__get_projects()
+        for project in projects:
+            output_path = '%s/%s' % (project['project_path'], self.output_path)
+            self.logger.debug('Looking for artifacts in %s' % output_path)
+            data = glob('%s/*' % output_path)
+            if data:
+                artifacts.append([ project['project'], data ])
         return artifacts
+
+    def __get_specs(self):
+        """
+        Returns a list of nuget spec files
+        """
+        specs = []
+        for _project in self.__get_projects():
+            project = _project['project']
+            project_path = _project['project_path']
+            self.logger.debug('Looking for nuspec in %s' % project_path)
+            data = glob('%s/*.nuspec' % project_path)
+            if data:
+                specs.append([project, data])
+        return specs
+
+    def list_projects(self, **kwargs):
+        """
+        Returns a list of all projects
+        """
+        return '\n'.join([ p['project'] for p in self.__get_projects() ])
+
+    def list_project_version(self, project_name, **kwargs):
+        """
+        Get the project's AssemblyVersion.
+        """
+        project = self.__get_project(project_name)
+        if not project:
+            raise Exception('Project not found in Solution file.')
+        filename = '%s/Properties/AssemblyInfo.cs'
+        f = open(filename % project['project_path'], 'r').read()
+        for line in f.split('\n'):
+            s = search('^\[assembly\: AssemblyVersion\("(.*)"\)\]', line)
+            if s:
+                return s.group(1)
+
+    def list_artifacts(self, **kwargs):
+        """
+        Returns a list of all solutions in project_path
+        """
+        _artifacts = self.__get_artifacts()
+        for project, artifacts in _artifacts:
+            print project
+            for artifact in artifacts:
+                print '  %s' % artifact.split('/')[-1]
+
+    def list_specs(self, **kwargs):
+        """
+        List all NuGet spec files found in projects.
+        """
+        _specs = self.__get_specs()
+        for project, specs in _specs:
+            print project
+            for spec in specs:
+                print '  %s' % spec.split('/')[-1]
+
+    def copy_artifacts(self, dest, **kwargs):
+        """
+        Copies the artifacts from a solutions file to destination directory.
+        """
+        dest = dest.rstrip('/')
+        if not os.path.exists(dest):
+            raise Exception('Does not appear %s exists' % dest)
+        if not os.path.isdir(dest):
+            raise Exception('It does not appear %s is a directory' % dest)
+
+        _artifacts = self.__get_artifacts()
+        for project_name, artifacts in _artifacts:
+            project_version = self.list_project_version(project_name)
+            dir_name = '%s/%s.%s' % (dest, project_name, project_version)
+
+            # Create the project directory if it does not exist.
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+
+            for artifact in artifacts:
+                filename = artifact.split('/')[-1]
+                self.logger.info('Copying %s to %s/' % (filename, dir_name))
+                shutil.copyfile(artifact, '%s/%s' % (dir_name, filename))
+
+    def clean(self, **kwargs):
+        """
+        Runs xbuild /t:clean to clear all artifacts.
+        """
+        cmd = '%s %s /t:clean' % (self.xbuild_path, self.solution_file)
+        self.logger.info('Running: %s' % cmd)
+        self.__log_run(self.__run(cmd, cwd=self.solution_path))
+
+    def nuget_restore(self, **kwargs):
+        """
+        Run Nuget.exe if we have a packages.json
+        """
+        cmd = '%s %s restore %s' % (
+                self.mono_path, self.nuget_path, self.solution_file
+        )
+        self.logger.info('Running: %s' % cmd)
+        self.logger.info('This can take some time...')
+        self.__log_run(self.__run(cmd, cwd=self.solution_path))
+
+    def xbuild(self, **kwargs):
+        """
+        Run xbuild on the solution
+        """
+        cmd = '%s %s' % (self.xbuild_path, self.solution_file)
+        self.logger.info('Running: %s' % cmd)
+        self.logger.info('This can take some time...')
+        self.__log_run(self.__run(cmd, cwd=self.solution_path))
 
 def get_version():
     """
@@ -116,17 +245,47 @@ def get_args():
     
     # Setup a basic parser for our input arguments.
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(title="Commands", metavar="--")
+    subparsers = parser.add_subparsers(title="Commands", metavar="")
     
     parser.add_argument('--version', action='version', version=get_version())
-    parser.add_argument('--debug', action='store_true', dest='debug', default=False) 
-    parser.add_argument('-p', dest='project_path', required=True, metavar='project_path', 
-            help='The relative path to your mono project.') 
+    parser.add_argument('--debug', action='store_true', dest='debug', default=False)
+    parser.add_argument('-s', dest='solution_file', metavar='solution_file', required=True,
+            help='A solution file to work with.')
 
     # Our subparser commands
-    list_solutions = subparsers.add_parser('listSolutions', 
-            help='Lists all solution files found in the project.')
-    list_solutions.set_defaults(method='get_solutions')
+    list_projects = subparsers.add_parser('listProjects',
+            help='Lists all project files found in the solution.')
+    list_projects.set_defaults(method='list_projects')
+
+    list_project_version = subparsers.add_parser('listProjectVersion',
+            help='Lists the projects Assembly Version.')
+    list_project_version.add_argument('project_name', help='A project in the solution file.')
+    list_project_version.set_defaults(method='list_project_version')
+
+    list_artifacts = subparsers.add_parser('listArtifacts',
+            help='Lists all artifacts files found in the project.')
+    list_artifacts.set_defaults(method='list_artifacts')
+
+    list_specs = subparsers.add_parser('listSpecs',
+            help='Lists all nuget spec files found in the project.')
+    list_specs.set_defaults(method='list_specs')
+
+    copy_artifacts = subparsers.add_parser('copyArtifacts',
+            help='Copies all artifacts files found in the project to directory.')
+    copy_artifacts.add_argument('dest', help='The destination directory.')
+    copy_artifacts.set_defaults(method='copy_artifacts')
+
+    clean = subparsers.add_parser('clean',
+            help='Runs xbuild clean, this will delete all artifacts.')
+    clean.set_defaults(method='clean')
+
+    nuget_restore = subparsers.add_parser('nugetRestore',
+            help='Run nuget restore on a solution file.')
+    nuget_restore.set_defaults(method='nuget_restore')
+
+    xbuild = subparsers.add_parser('xbuild',
+            help='xbuild on a solution file.')
+    xbuild.set_defaults(method='xbuild')
 
     # parse our parsers and get the args.
     args = parser.parse_args()
@@ -138,19 +297,15 @@ def main():
     """
     args = get_args()
 
-    # We should be sure the project path exists.
-    if os.path.exists(args.project_path):
-        project_path = os.path.expanduser(args.project_path)
-    else:
-        raise Exception('%s does not exists' % args.project_path)
-
     # create a new MonoTool object using our project path.
-    project = MonoTool(project_path, debug=args.debug)
+    mt = MonoTool(args.solution_file, debug=args.debug)
 
-    method = getattr(project, args.method)
-    print method()
+    method = getattr(mt, args.method)
+    res = method(**args.__dict__)
+    if res:
+        print res
 
-      
 
 if __name__ == '__main__':
     main()
+
